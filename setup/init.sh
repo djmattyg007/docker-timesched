20a57853c0152810193a096645e60f331cef223c
Matthew Gamble <git@matthewgamble.net>
2016-09-26 23:58:20 +1000

    Various fixes

--- setup/init.sh
+++ setup/init.sh
@@ -5,17 +5,18 @@ if [[ -z "${TIMESCHED_HTTP_SERVERNAME}${TIMESCHED_HTTPS_SERVERNAME}" ]]; then
     exit 1
 fi
 
+mkdir /etc/nginx/sites-enabled
 if [[ -n "${TIMESCHED_HTTP_SERVERNAME}" ]]; then
     echo "[info] Found HTTP servername, adding HTTP configuration to nginx"
-    sed -e "s/___SERVER_NAME___/${TIMESCHED_HTTP_SERVERNAME}/" /etc/timesched/nginx/http.ngx > /etc/nginx/sites-available/http.ngx
+    sed -e "s/___SERVER_NAME___/${TIMESCHED_HTTP_SERVERNAME}/" /etc/timesched/nginx/http.ngx > /etc/nginx/sites-enabled/http.ngx
 else
-    rm -f /etc/nginx/sites-available/http.ngx
+    rm -f /etc/nginx/sites-enabled/http.ngx
 fi
 if [[ -n "${TIMESCHED_HTTPS_SERVERNAME}" ]]; then
     echo "[info] Found HTTPS servername, adding HTTPS configuration to nginx"
-    sed -e "s/___SERVER_NAME___/${TIMESCHED_HTTPS_SERVERNAME}/" /etc/timesched/nginx/https.ngx > /etc/nginx/sites-available/https.ngx
+    sed -e "s/___SERVER_NAME___/${TIMESCHED_HTTPS_SERVERNAME}/" /etc/timesched/nginx/https.ngx > /etc/nginx/sites-enabled/https.ngx
 else
-    rm -f /etc/nginx/sites-available/https.ngx
+    rm -f /etc/nginx/sites-enabled/https.ngx
 fi
 
 # Finally, run nginx.
--- setup/root/install.sh
+++ setup/root/install.sh
@@ -8,6 +8,9 @@ source /root/functions.sh
 # Install any packages
 echo "Installing nginx..."
 pacman -S --noconfirm nginx
+mkdir /var/log/nginx && chmod 775 /var/log/nginx && chown http:http /var/log/nginx
+ln -sf /dev/stdout /var/log/nginx/access.log
+ln -sf /dev/stderr /var/log/nginx/error.log
 
 echo "Downloading timesched v${TIMESCHED_VERSION} tarball"
 curl -L -sS --create-dirs -o /tmp/timesched/timesched.tar.gz https://github.com/djmattyg007/timesched/releases/download/djcustom-v${TIMESCHED_VERSION}/djcustom-v${TIMESCHED_VERSION}-release.tar.gz
